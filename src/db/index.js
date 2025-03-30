9b14b97a8228769e098279c66ab197b2f13ee15b
Saim ALi <saim.ali.siddiqui2004@gmail.com>
2025-03-30 06:06:49 +0500

    v1

--- src/db/index.js
+++ src/db/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/minijob`)
+        const connectionInstance = await mongoose.connect(process.env.MONGODB_URI)
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("MongoDB connection FAILED ", error);
